c4ed542ef590d4eb8b61629908befe41f78af431
MironaAloneh <mironaaloneh@gmail.com>
2024-12-07 02:08:19 +0600

    edit a init and main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
+import asyncio
 from fastapi import FastAPI
-
 from api.routes.api import router as api_router
 from core.events import create_start_app_handler
 from core.config import API_PREFIX, DEBUG, PROJECT_NAME, VERSION
-
+from api import start_telegram_bot  # Импорт функции для запуска бота
 
 def get_application() -> FastAPI:
     application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
@@ -13,5 +13,10 @@ def get_application() -> FastAPI:
         application.add_event_handler("startup", create_start_app_handler(application))
     return application
 
-
 app = get_application()
+
+# Запуск Telegram-бота
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(start_telegram_bot())
+
