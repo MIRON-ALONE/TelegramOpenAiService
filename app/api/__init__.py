a51067b349087177a2dc1dd52358056d7c4ac38a
MironaAloneh <mironaaloneh@gmail.com>
2024-12-06 17:23:01 +0600

    create a "HelloWorld" messasge for take a test

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,13 @@
-# mpmqtcc
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("7616064677:AAHzFDKNoB5Qo8hrz33oEeQlsF7jRO5jSFk").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
